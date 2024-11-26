a0fab6f94984e8f25cbc268e7c6d54bcc961619d
veerLakshya <lakshyaveer928@gmail.com>
2024-11-26 18:42:57 +0530

    corrected imports in home

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -4,7 +4,7 @@ import ActionButton from "../../shared/ActionButton";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import HomePageText from "../../assets/HomePageText.png";
 import HomePageGraphic from "../../assets/HomePageGraphic.png";
-import SponsorRedbull from "../../assets/SponsorRedbull.png";
+import SponsorRedbull from "../../assets/SponsorRedBull.png";
 import SponsorForbes from "../../assets/SponsorForbes.png";
 import SponsorFortune from "../../assets/SponsorFortune.png";
 import { motion } from "framer-motion";
